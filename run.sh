cb5d016bc2bacac9efa0de8f75f66614ef2432c8
Alexander Strack <strack.as@icloud.com>
2023-09-12 13:56:23 +0200

    path adjustments

--- run.sh
+++ run.sh
@@ -8,14 +8,14 @@ set +x
 # Variables
 ################################################################################
 # Set MKL directory
-export MKL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/../hpxsc_installations/hpx_mkl_v1.8.1/build/mkl/mkl/2023.0.0/lib"
+export MKL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/../hpxsc_installations/hpx_mkl_gcc_v1.8.1/build/mkl/mkl/2023.0.0/lib/cmake/mkl"
 # Configure MKL
 export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic -DMKL_INTERFACE_FULL=intel_lp64 -DMKL_THREADING=sequential'
 
 ################################################################################
 # Compile code
 ################################################################################
-rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release --DMKL_DIR="${MKL_DIR}/cmake/mkl" ${MKL_CONFIG} && make all
+rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DMKL_DIR="${MKL_DIR}" ${MKL_CONFIG} && make all
 
 ################################################################################
 # Run BLAS benchmark
@@ -23,4 +23,4 @@ rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -
 OUTPUT_FILE_BLAS="runtimes_mkl.txt"
 rm $OUTPUT_FILE_BLAS
 touch $OUTPUT_FILE_BLAS
-../build/hpx_blas | tee $OUTPUT_FILE_BLAS
+./mkl_benchmark | tee $OUTPUT_FILE_BLAS
